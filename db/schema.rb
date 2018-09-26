a766656d808d77863d6d4bb2dbf2da5f77efcf3b
ryannamgung <ryannamgung@gmail.com>
2018-09-26 03:07:51 -0400

    updated database

---
+++ db/schema.rb
@@ -0,0 +1,58 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 2018_09_25_173539) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "companies", force: :cascade do |t|
+    t.string "name"
+    t.string "password"
+    t.string "motto"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "items", force: :cascade do |t|
+    t.string "name"
+    t.string "image_src"
+    t.string "good_type"
+    t.integer "price"
+    t.boolean "stock"
+    t.bigint "company_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["company_id"], name: "index_items_on_company_id"
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.string "comment"
+    t.integer "rating"
+    t.bigint "user_id"
+    t.bigint "item_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_reviews_on_user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "password"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_foreign_key "items", "companies"
+  add_foreign_key "reviews", "users"
+  add_foreign_key "reviews", "items"
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -12,7 +12,11 @@ Company.create(name: "the corp", password: "confusion", motto: "you'll never see
 
 Item.create(name: "black hat", image_src: "https://images-na.ssl-images-amazon.com/images/I/711c4BhJ7PL._SL1500_.jpg", good_type: "hat", price: 30, stock: true, company_id: 1)
 
-Item.create(name: "shoes", image_src: "https://sneakernews.com/wp-content/uploads/2017/07/dwyane-wade-li-ning-way-of-wade-6-unveiled-01.jpg?w=540&h=380&crop=1", good_type: "shoes", price: 100, stock: true, company_id: 2)
+Item.create(name: "sneakers", image_src: "https://sneakernews.com/wp-content/uploads/2017/07/dwyane-wade-li-ning-way-of-wade-6-unveiled-01.jpg?w=540&h=380&crop=1", good_type: "shoes", price: 100, stock: true, company_id: 2)
+
+Item.create(name: "white t-shirt", image_src: "https://i.ebayimg.com/images/g/vTwAAOxydlFS-loL/s-l300.jpg", good_type: "shirt", price: 10, stock: true, company_id: 1)
+
+Item.create(name: "Air Jordans 1", image_src: "https://cdn5.kicksonfire.com/wp-content/uploads/2015/06/Air-Jordan-1-Pinnacle-3.jpg?x23094", good_type: "shoes", price: 200, stock: true, company_id: 1)
 
 Review.create(user_id: 1, item_id: 1, comment: "A very good hat!", rating: 3)
-Review.create(user_id: 2, item_id: 2, comment: "Dwayne Wade's shoes are better than Micheal Jordan's", rating: 5)
+Review.create(user_id: 2, item_id: 1, comment: "Dwayne Wade's shoes are better than Micheal Jordan's", rating: 5)
